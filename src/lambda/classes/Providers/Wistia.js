dddf1b740a2e78accb9af112164c86b4c5405cdc
Marco Montalbano <marcomontalbano.work@gmail.com>
2020-08-25 20:03:28 +0200

    Add Wistia video support (#15)

---
+++ src/lambda/classes/Providers/Wistia.js
@@ -0,0 +1,36 @@
+import VideoProvider from '../VideoProvider';
+import fetch from '../proxiedFetch';
+
+// https://wistia.com/support/developers/oembed#an-example
+
+export default class Wistia extends VideoProvider {
+
+    get providerName() {
+        return 'wistia';
+    }
+
+    static get regex() {
+        return [
+            // - //home.wistia.com/medias/e4a27b971d
+            /https?:\/\/(?:[^.]+\.)?(?:wistia\.com|wi\.st)\/medias\/([a-zA-Z0-9\_\-]+)/,
+
+            // - //home.wistia.com/embed/iframe/e4a27b971d
+            /https?:\/\/(?:[^.]+\.)?(?:wistia\.com|wi\.st)\/embed\/[^\/]+\/([a-zA-Z0-9\_\-]+)/,
+
+            // - //wistia.com/series/one-ten-one-hundred?wchannelid=z2vptfjlxk&wvideoid=donagpxtdr
+            /https?:\/\/(?:[^.]+\.)?(?:wistia\.com|wi\.st)\/.*wvideoid=([a-zA-Z0-9\_\-]+)/,
+        ];
+    }
+
+    static get useCloudinary() {
+        return false
+    }
+
+    getThumbnail_asVideoUrl() {
+        const endpoint = `https://fast.wistia.com/oembed?url=http%3A%2F%2Fhome.wistia.com%2Fmedias%2F${this.getId()}`;
+
+        return fetch(endpoint)
+            .then(response => response.json())
+            .then(json => `${json.thumbnail_url}${this.options.showPlayIcon ? '&image_play_button=1' : ''}`)
+    }
+}
