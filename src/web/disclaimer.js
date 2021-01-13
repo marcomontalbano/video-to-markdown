580c5700b668f4c1d8ead972a4d8ab76ebfcfe97
Marco Montalbano <marcomontalbano.work@gmail.com>
2021-01-13 23:06:22 +0100

    Restructure folder files

---
+++ src/web/disclaimer.js
@@ -0,0 +1,28 @@
+const closeModal = (event) => {
+    const isDonation = event.target.closest('.donations');
+    const isClose = isDonation && event.target.matches('.close');
+
+    if (isClose || !isDonation) {
+        document.body.classList.remove('modal');
+        window.scrollTo(0, 0);
+        sessionStorage.setItem('prompt-donation', 'false');
+    }
+}
+
+export const load = () => {
+    const donationSection = document.querySelector('.container.donations');
+
+    if (sessionStorage.getItem('prompt-donation') !== 'false') {
+        document.body.classList.add('modal');
+    
+        document.addEventListener('click', closeModal)
+    
+        setTimeout(() => {
+            const outFromTop = donationSection.offsetTop - window.pageYOffset < 0;
+            const outFromBottom = (donationSection.offsetTop + donationSection.offsetHeight) > window.innerHeight;
+            if (outFromTop || outFromBottom) {
+                window.scrollTo(0, donationSection.offsetTop);
+            }
+        }, 100)
+    }
+}
