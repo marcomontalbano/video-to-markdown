0427d6239afb625aad2dbaeb238257ada26b99b9
Marco Montalbano <marcomontalbano.work@gmail.com>
2022-11-25 21:27:24 +0100

    Update dependencies to latest

--- .posthtmlrc.js
+++ .posthtmlrc.js
@@ -2,6 +2,12 @@ module.exports = {
     plugins: [
         require('posthtml-include')({
             root: require('path').join(__dirname, 'src')
+        }),
+        require('posthtml-expressions')({
+            locals: {
+                NODE_ENV: process.env.NODE_ENV,
+                GA_TRACKING_ID: process.env.GA_TRACKING_ID,
+            }
         })
     ]
 };
