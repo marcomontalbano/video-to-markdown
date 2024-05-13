dad30d001ec265d3d46b757ad621bdafe836b227
Marco Montalbano <marcomontalbano.work@gmail.com>
2024-05-13 22:09:25 +0200

    fix: use OneDrive api to get the thumbnail

--- netlify/functions/classes/Providers/OneDrive.js
+++ netlify/functions/classes/Providers/OneDrive.js
@@ -1,6 +1,5 @@
 import VideoProvider from '../VideoProvider.js';
 import fetch from '../proxiedFetch.js';
-import htmlMiner from 'html-miner';
 
 // https://onedrive.live.com/
 
@@ -18,7 +17,44 @@ export default class OneDrive extends VideoProvider {
 
   getThumbnail_asVideoUrl() {
     return fetch(this.url)
-      .then((response) => response.text())
-      .then((html) => htmlMiner(html, (arg) => arg.$('[property="og:image"]').attr('content')));
+      .then((response) => response.url)
+      .then((redirectUrl) => {
+        this.log('redirectUrl', redirectUrl);
+
+        const searchParams = new URL(redirectUrl).searchParams;
+
+        // GET /drives/{drive-id}/items/{item-id}/thumbnails
+        const driveId = searchParams.get('cid');
+        const itemId = searchParams.get('resId');
+        const authkey = searchParams.get('authkey');
+
+        if (driveId == null || itemId == null || authkey == null) {
+          throw new Error('Link is not valid.');
+        }
+
+        // https://learn.microsoft.com/en-us/onedrive/developer/rest-api/api/driveitem_list_thumbnails?view=odsp-graph-online
+        const apiUrl = `https://api.onedrive.com/v1.0/drives/${driveId}/items/${itemId}/thumbnails?authkey=${authkey}`;
+        this.log('apiUrl', apiUrl);
+        return fetch(apiUrl);
+      })
+      .then((res) => res.json())
+      .then((json) => {
+        const [firstThumbnail] = json.value;
+
+        if (firstThumbnail == null) {
+          throw new Error('Cannot find a thumbnail.');
+        }
+
+        const { large, medium, small } = firstThumbnail;
+        const thumbnailUrl = large?.url ?? medium?.url ?? small?.url;
+
+        if (firstThumbnail == null) {
+          throw new Error('Cannot find a thumbnail URL.');
+        }
+
+        this.log('thumbnailUrl', thumbnailUrl);
+
+        return thumbnailUrl;
+      });
   }
 }
